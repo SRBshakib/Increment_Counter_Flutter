cc61a30aa5d898b985f78cf947fa6282a8634a21
Shakibur Rahman Bhuiyan <srbshakib1@gmail.com>
2025-08-01 21:05:22 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Simple Counter',
       theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // This is the theme of your application.
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue, // A simpler way to set the primary color theme
       ),
-      home: const MyHomePage(title: 'Increment Counter'),
+      home: const MyHomePage(title: 'Simple Counter Home Page'),
     );
   }
 }
@@ -24,7 +31,14 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
 
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
   final String title;
 
@@ -33,80 +47,94 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 0; // This variable stores the current count.
 
   void _incrementCounter() {
+    // setState is a method that tells Flutter that something in the State has changed,
+    // which causes it to rerun the build method below so that the display can
+    // reflect the updated values. If we changed _counter without calling
+    // setState(), then the build method would not be called again, and so
+    // nothing would appear to happen.
     setState(() {
-
-      _counter++;
+      _counter++; // Increase the counter by one.
     });
   }
 
   void _decrementCounter() {
     setState(() {
-      if (_counter > 0) {
-        _counter--;
+      if (_counter > 0) { // Only decrement if counter is greater than 0
+        _counter--; // Decrease the counter by one.
       }
     });
   }
 
-  void _reset(){
+  void _resetCounter() {
     setState(() {
-      if(_counter >= 0){
-        _counter = 0;
-      }
+      _counter = 0; // Reset the counter to zero.
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center, // Center the children vertically.
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-
+            const Text(
+              'You have pushed the button this many times:',
+            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              '$_counter', // Display the current counter value.
+              style: Theme.of(context).textTheme.headlineMedium, // Use a predefined text style from the theme.
             ),
           ],
         ),
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-        // Decrement Button
-        FloatingActionButton(
-        onPressed: _decrementCounter,
-        tooltip: 'Decrement',
-        child: const Icon(Icons.remove),
-      ),
-          const SizedBox(width: 20),
+      floatingActionButton: Column( // Use a Column to stack multiple FABs vertically
+        mainAxisAlignment: MainAxisAlignment.end, // Align FABs to the bottom
+        children: <Widget>[
           FloatingActionButton(
-            onPressed: _reset,
-            tooltip: 'Reset',
-            child: const Icon(Icons.refresh),
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add), // Simple add icon.
           ),
-      const SizedBox(width: 20), // Space between buttons
-      // Increment Button
-      FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-
-    ],
+          const SizedBox(height: 10), // Some spacing between buttons
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            backgroundColor: Colors.orange, // Give it a different color
+            child: const Icon(Icons.remove), // Simple remove icon.
+          ),
+          const SizedBox(height: 10), // Some spacing
+          FloatingActionButton(
+            onPressed: _resetCounter,
+            tooltip: 'Reset',
+            backgroundColor: Colors.red, // Give it another color
+            child: const Icon(Icons.refresh), // Simple refresh icon.
+          ),
+        ],
       ),
     );
   }
